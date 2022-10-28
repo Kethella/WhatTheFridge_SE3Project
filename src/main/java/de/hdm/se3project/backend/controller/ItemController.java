a2bfba471df5a87c6c54adc63176a40872ed502d
Kethella Oliveira <kethella.oliveira@gmail.com>
2022-10-28 16:36:09 +0200

    Controller FridgeItems V0

---
+++ src/main/java/de/hdm/se3project/backend/controller/ItemController.java
@@ -0,0 +1,21 @@
+package de.hdm.se3project.backend.controller;
+
+import de.hdm.se3project.backend.exception.ResourceNotFoundException;
+import de.hdm.se3project.backend.model.Recipe;
+//import de.hdm.se3project.backend.repository.ItemRepository;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/api/v1")
+
+public class ItemController {
+
+
+}
