d8ed3fbe703872005aa52f2618b160c5f541641a
Kethella Oliveira <kethella.oliveira@gmail.com>
2022-10-28 16:29:41 +0200

    Making structural changes according to Maven and MongoDB new conf

--- src/main/java/de/hdm/se3project/backend/model/Account.java
+++ src/main/java/de/hdm/se3project/backend/model/Account.java
@@ -4,12 +4,11 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.annotation.Id;
 
 
-@Document(collection = "Account")
+@Document(collection = "accounts")
 public class Account {
 
     @Id
     private String id;
-
     private String name;
     private String email;
     private String password;
