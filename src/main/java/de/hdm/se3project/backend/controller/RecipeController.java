d356f442dee4de33f8f5168c205e84102ee146d2
Nadine Eunuos <ne035@hdm-stuttgart.de>
2022-10-28 12:28:06 +0200

    Create Recipe and RecipeController

--- src/main/java/de/hdm/se3project/backend/controller/RecipeController.java
+++ src/main/java/de/hdm/se3project/backend/controller/RecipeController.java
@@ -1,5 +1,41 @@
 package de.hdm.se3project.backend.controller;
 
-//TODO
+import de.hdm.se3project.backend.exception.ResourceNotFoundException;
+import de.hdm.se3project.backend.model.Recipe;
+import de.hdm.se3project.backend.repository.RecipeRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
 public class RecipeController {
+
+    private final RecipeRepository repository;
+
+    public RecipeController(RecipeRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/recipes")
+        //@CrossOrigin
+    List<Recipe> getAllRecipes(){
+        return repository.findAll();
+    }
+
+    @GetMapping("/recipes/{id}")
+        //@CrossOrigin
+    Recipe getOneRecipe(@PathVariable String id) throws ResourceNotFoundException {
+        /*Account account = accountRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Account not found for this id :: " + id));
+        return ResponseEntity.ok().body(account);*/
+
+        return repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Account not found for this id :: " + id));
+    }
+
+
 }
