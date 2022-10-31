6db1bf63c7b76e026d3ce82e48ffd51df3a49e3c
Nadine Eunuos <ne035@hdm-stuttgart.de>
2022-10-31 14:17:23 +0100

    Create Category Enum Class. Attempt Get Request for testing

--- src/main/java/de/hdm/se3project/backend/controller/RecipeController.java
+++ src/main/java/de/hdm/se3project/backend/controller/RecipeController.java
@@ -2,6 +2,7 @@ package de.hdm.se3project.backend.controller;
 
 import de.hdm.se3project.backend.exception.ResourceNotFoundException;
 import de.hdm.se3project.backend.model.Recipe;
+import de.hdm.se3project.backend.model.enums.Category;
 import de.hdm.se3project.backend.repository.RecipeRepository;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,13 +27,21 @@ public class RecipeController {
     @GetMapping("/recipes/{id}")
         //@CrossOrigin
     Recipe getOneRecipe(@PathVariable String id) throws ResourceNotFoundException {
-        /*Account account = accountRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Account not found for this id :: " + id));
-        return ResponseEntity.ok().body(account);*/
 
         return repository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Account not found for this id :: " + id));
     }
+/*
+    //temporary for testing
+    @GetMapping("/recipes/category/{id}")
+    String getCategoryText(@PathVariable String id) throws ResourceNotFoundException {
+        Category category = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Category not found for this id : " + id));
+
+        return category.getCategory().getText();
+    }
+
+*/
 
     @PostMapping("/recipes")
         //@CrossOrigin
