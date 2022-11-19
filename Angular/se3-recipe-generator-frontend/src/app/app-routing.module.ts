5b21f57586241f514ec25507ab59c90f9bca2d25
Milena Hristova <mh366@hdm-stuttgart.de>
2022-11-19 01:31:30 +0100

    navbar component w/ (dummy) routing #36

--- Angular/se3-recipe-generator-frontend/src/app/app-routing.module.ts
+++ Angular/se3-recipe-generator-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DummyPagesComponent } from './components/dummy-pages/dummy-pages.component';
 
-const routes: Routes = [];
+//TODO: Replace with actual paths, change routerLinks in navbar component
+const routes: Routes = [
+  {path: 'home', component: DummyPagesComponent},
+  {path: 'fridge', component: DummyPagesComponent},
+  {path: 'my_recipes', component: DummyPagesComponent},
+  {path: 'fav_recipes', component: DummyPagesComponent},
+  {path: 'account_settings', component: DummyPagesComponent},
+  {path: 'sign_in', component: DummyPagesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Angular/se3-recipe-generator-frontend/src/app/app.module.ts
+++ Angular/se3-recipe-generator-frontend/src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatToolbarModule, MatButtonModule, MatIconModule } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AccountComponent } from './components/account/account.component'
+import { AccountComponent } from './components/account/account.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import {MatMenuModule} from '@angular/material/menu';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { DummyPagesComponent } from './components/dummy-pages/dummy-pages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AccountComponent
+    AccountComponent,
+    NavbarComponent,
+    DummyPagesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule 
+    HttpClientModule,
+    MatToolbarModule, 
+    MatButtonModule,
+    MatIconModule, 
+    MatMenuModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
