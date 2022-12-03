e597268e476535259d39a338a6b2f0abc29e914f
Aleksandra Gidionova <ag186@hdm-stuttgart.de>
2022-12-03 18:53:50 +0100

    version updates

--- Angular/se3-recipe-generator-frontend/src/app/app.module.ts
+++ Angular/se3-recipe-generator-frontend/src/app/app.module.ts
@@ -1,22 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatToolbarModule, MatButtonModule, MatIconModule } from '@angular/material';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AccountComponent } from './components/account/account.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import {MatMenuModule} from '@angular/material/menu';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { DummyPagesComponent } from './components/dummy-pages/dummy-pages.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { AccountComponent } from './components/account/account.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DummyPagesComponent } from './components/dummy-pages/dummy-pages.component';
 import { SignUpComponent } from './components/account/sign-up/sign-up.component';
 import { LoginComponent } from './components/account/login/login.component';
-import {MatStepperModule} from '@angular/material/stepper';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatSelectModule} from '@angular/material/select';
-import { MatInputModule } from '@angular/material';
+
+import { PersonalMaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -31,17 +29,10 @@ import { MatInputModule } from '@angular/material';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    MatToolbarModule, 
-    MatButtonModule,
-    MatIconModule, 
-    MatMenuModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
     FormsModule,
-    MatStepperModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatInputModule
+    PersonalMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
