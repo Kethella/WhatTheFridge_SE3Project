a3d8e16ff2d576fa89eeb64aa68847ef99767765
Kethella Oliveira <kethella.oliveira@gmail.com>
2023-01-03 20:26:56 +0100

    keeping one test class only for verification

--- src/test/java/de/hdm/se3project/backend/controller/integrationTest/repositoryTest/AccountRepositoryTest.java
+++ src/test/java/de/hdm/se3project/backend/controller/integrationTest/repositoryTest/AccountRepositoryTest.java
@@ -1,57 +1,52 @@
 package de.hdm.se3project.backend.controller.integrationTest.repositoryTest;
 
+import de.hdm.se3project.backend.Application;
 import de.hdm.se3project.backend.model.Account;
 import de.hdm.se3project.backend.model.enums.SecurityQuestion;
 import de.hdm.se3project.backend.repository.AccountRepository;
 import jdk.jfr.Description;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 
 import java.io.IOException;
 import java.net.Socket;
 import java.util.List;
 
-@Testcontainers//need to enable to run the tc Junit 5 into test container mode --> it runs all containers annotated with @container
-@DataMongoTest(excludeAutoConfiguration = EmbeddedMongoAutoConfiguration.class)
+// @Testcontainers//need to enable to run the tc Junit 5 into test container mode --> it runs all containers annotated with @container
+// @DataMongoTest(excludeAutoConfiguration = EmbeddedMongoAutoConfiguration.class)
+@SpringBootTest
+@TestPropertySource(locations = "/application-test.properties")
 public class AccountRepositoryTest {
 
     @Autowired
     private AccountRepository accountRepository;  //getting instance of the Account Repository
 
-    @Container  //creating a mongoDB container obj and keeping it until the end of all tests, then it will be deleted
-    public static MongoDBContainer container = new MongoDBContainer(DockerImageName.parse("mongo:latest")); //class MongoDBContainer coming through library passing desired Docker image
-
-    @DynamicPropertySource //Connecting to our local dockerized MongoDB instance
-    public static void setProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.data.mongodb.uri", container::getReplicaSetUrl);
-    }
-
-    @BeforeAll //starting the container
-    static void initAll(){
-        container.start();
-    }
-
+//    @Container  //creating a mongoDB container obj and keeping it until the end of all tests, then it will be deleted
+//    public static MongoDBContainer container = new MongoDBContainer(DockerImageName.parse("mongo:latest")); //class MongoDBContainer coming through library passing desired Docker image
+//
+//    @DynamicPropertySource //Connecting to our local dockerized MongoDB instance
+//    public static void setProperties(DynamicPropertyRegistry registry) {
+//        registry.add("spring.data.mongodb.uri", container::getReplicaSetUrl);
+//    }
+//
+//    @BeforeAll //starting the container
+//    static void initAll(){
+//        //container.start();
+//    }
+//
     @AfterEach
     void cleanUp(){
         this.accountRepository.deleteAll();
     }
-
-    @Test
-    @Description("Check if Container has started and the public port is available")
-    void containerStartsAndPublicPortIsAvailable() {
-        assertThatPortIsAvailable(container);
-    }
+//
+//    @Test
+//    @Description("Check if Container has started and the public port is available")
+//    void containerStartsAndPublicPortIsAvailable() {
+//        assertThatPortIsAvailable(container);
+//    }
 
     @Test
     @Description("Verifies if container is empty before save Account")
