3e28ddb1f9a329b016713f25b401896a32c88b67
Kethella Oliveira <kethella.oliveira@gmail.com>
2023-01-04 12:38:26 +0100

    moved mongo service creation to unit test job

--- src/test/java/de/hdm/se3project/backend/controller/integrationTest/repositoryTest/FridgeItemRepositoryTest.java
+++ src/test/java/de/hdm/se3project/backend/controller/integrationTest/repositoryTest/FridgeItemRepositoryTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
@@ -23,6 +24,7 @@ import java.util.List;
 
 @Testcontainers//need to enable to run the tc Junit 5 into test container mode --> it runs all containers annotated with @container
 @DataMongoTest(excludeAutoConfiguration = EmbeddedMongoAutoConfiguration.class)
+//@SpringBootTest
 public class FridgeItemRepositoryTest {
 
     @Autowired
