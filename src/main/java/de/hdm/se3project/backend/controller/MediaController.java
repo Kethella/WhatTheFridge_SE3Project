06b65b42486c1e5c001cb920c12dbd07819d56a8
Aleksandra Gidionova <ag186@hdm-stuttgart.de>
2023-01-22 22:56:38 +0100

    fix

--- src/main/java/de/hdm/se3project/backend/controller/MediaController.java
+++ src/main/java/de/hdm/se3project/backend/controller/MediaController.java
@@ -14,7 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/api/v1/media")
+@RequestMapping("/media")
 @CrossOrigin(origins = "http://localhost:4200")
 public class MediaController {
 
